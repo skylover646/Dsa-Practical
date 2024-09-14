84bc66099f72787778957f0ecdbdba81b22d9d26
skylover646 <akpatel64000@gmail.com>
2024-09-14 21:30:25 +0530

    files

--- sumof2numbers.cpp
+++ sumof2numbers.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 int main()
 {
-int a=5, b=4;
-cout<<"sum="<<(a+b)<<endl;
+    int a, b;
+cout << "enter a:"<<endl;
+cin>>a;
+cout<<"enter b:"<<endl;
+cin>>b;
+int sum=a+b;
+cout <<sum<<endl;
+//or write like uper code int a=5, b=4;
+//cout<<"sum="<<(a+b)<<endl;
     return 0;
 }
