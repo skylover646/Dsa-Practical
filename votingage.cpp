46287824c540a09dc12f8b0985337307626e4b7f
skylover646 <akpatel64000@gmail.com>
2024-09-15 22:53:47 +0530

    vote age

---
+++ votingage.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"enter age :";
+    cin>>age;
+    if(age >=18){
+        cout << "you can vot\n";
+
+    }
+    else{
+        cout<<"you can't vote\n";
+    }
+}
