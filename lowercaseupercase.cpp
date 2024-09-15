72f98070979ce5ee2dbd092ed09eb2e523329f0d
skylover646 <akpatel64000@gmail.com>
2024-09-15 23:32:56 +0530

    lowerupercase

---
+++ lowercaseupercase.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char ch;
+cout <<"enter char :";
+cin >>ch;
+if (ch>=65 && ch <=97){
+    cout <<"upercase\n";
+}else {
+    cout<<"lowercase\n";
+}
+
+    return 0;
+}
