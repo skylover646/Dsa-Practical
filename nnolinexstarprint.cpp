7524e8b898f5f50c39e5fb5717cbbe6410e4238f
skylover646 <akpatel64000@gmail.com>
2024-09-17 20:48:17 +0530

    modify file

--- nnolinexstarprint.cpp
+++ nnolinexstarprint.cpp
@@ -5,7 +5,7 @@ int main()
     int n=6;
     for(int i=1;i<=n;i++){
         int m=6;
-        for(int i=1;i<=m;i++){
+        for(int j=1;j<=m;j++){
             cout<<"*";
         }
         cout<<endl;
