95e1ff659a0d15f8150c2ecb9f124484407a7d2a
skylover646 <akpatel64000@gmail.com>
2024-09-17 20:45:26 +0530

    one more file add

---
+++ nnolinexstarprint.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=6;
+    for(int i=1;i<=n;i++){
+        int m=6;
+        for(int i=1;i<=m;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
