07d0d0d883b616e54a72919acb89f117da04783a
skylover646 <akpatel64000@gmail.com>
2024-09-19 00:18:36 +0530

    modify file

--- squarepattern01.cpp
+++ squarepattern01.cpp
@@ -3,8 +3,8 @@ using namespace std ;
 int main()
 {
 int n=4;
-for(int i=1;i<=n;i++){
-    for(int j=1;j<=n;j++){
+for(int i=1;i<=n;i++){//outer loop
+    for(int j=1;j<=n;j++){//inner loop
         cout<<j;
     }
     cout<<endl;
